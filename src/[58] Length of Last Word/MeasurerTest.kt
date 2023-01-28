ac839f13d987c30ed23dd92a43683e5c00b10a63
Di.Est <di.est.52@gmail.com>
2023-01-28 17:28:29 +0300

    task 58 tests for Measurer

---
+++ src/[58] Length of Last Word/MeasurerTest.kt
@@ -0,0 +1,25 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * @author Demitrist on 28.01.2023
+ */
+
+class MeasurerTest{
+
+    @Test
+    fun `test correct result`(){
+        val measurer = Measurer.Base(delimiter = " ")
+        val expected = listOf(0,5,5,6,2,15)
+        val strings = listOf(
+            "",
+            "test stair of ant",
+            "somme",
+            "me an     Philip",
+            "a at b",
+            "djf   hdhshh hhhhhhhhhhhhhhh sa ssafsadf  384hsd")
+        strings.forEachIndexed { index, str ->
+            assertEquals(expected[index], measurer.size(string = str))
+        }
+    }
+}
