3b934f4aa6a3936e49810086f47cb67505cb72c0
Di.Est <di.est.52@gmail.com>
2023-01-31 23:10:34 +0300

    task 14: tests for prefix finder

---
+++ src/[14] Longest Common Prefix/Tests/PrefixFinderTest.kt
@@ -0,0 +1,74 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * @author Demitrist on 30.01.2023
+ */
+
+class PrefixFinderTest {
+
+    @Test
+    fun `test correct`() {
+        val arrayValidator = FakeValidator(result = true)
+        val finder = PrefixFinder.Base(validator = arrayValidator)
+        val input = listOf(
+            arrayOf("flower","flow","flight"),
+            arrayOf("car", "cat", "can't"),
+            arrayOf("help","head","door","draft","dart"),
+            arrayOf("craft", "fast", "food"),
+            arrayOf("health", "count", "read"),
+            arrayOf("p"),
+            arrayOf("a")
+        )
+        val expected = listOf(
+            "fl",
+            "ca",
+            "",
+            "",
+            "",
+            "p",
+            "a"
+        )
+
+        input.forEachIndexed { index, strings ->
+            var actual: String = finder.prefix(strings)
+            var exp: String = expected[index]
+            assertEquals(exp, actual)
+        }
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `test incorrect input`() {
+        val arrayValidator = FakeValidator(result = false)
+        val finder = PrefixFinder.Base(validator = arrayValidator)
+        val input = listOf(
+            arrayOf("cat5", "ta5ka", "1rat"),
+            arrayOf("test$", "cat dog", ".front."),
+            arrayOf("React", "newImage", "CARROT"),
+            emptyArray(),
+            Array(size = 201) { "" },
+            arrayOf(
+                "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "qazwsxedcrfvtgbyhnuj" +
+                        "a"
+            )
+        )
+
+        input.forEach {
+            finder.prefix(it)
+        }
+    }
+}
+
+private class FakeValidator(private val result: Boolean = true) : ArrayValidator {
+
+    override fun isValid(strings: Array<String>): Boolean = result
+}
